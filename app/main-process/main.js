18c0e9492340f4fc7e6a4752b94f1a1963ee44a2
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2023-12-21 21:34:21 +0500

    inky commit

---
+++ app/main-process/main.js
@@ -0,0 +1,258 @@
+const electron = require('electron');
+const { fstat } = require('original-fs');
+const app = electron.app
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+const ProjectWindow = require("./projectWindow.js").ProjectWindow;
+const DocumentationWindow = require("./documentationWindow.js").DocumentationWindow;
+const AboutWindow = require("./aboutWindow.js").AboutWindow;
+const AppMenus = require('./appmenus.js');
+const forceQuitDetect = require('./forceQuitDetect');
+const Inklecate = require("./inklecate.js").Inklecate;
+const fs = require("fs");
+
+function inkJSNeedsUpdating() {
+    return false;
+    // dialog.showMessageBox({
+    //   type: 'error',
+    //   buttons: ['Okay'],
+    //   title: 'Export for web unavailable',
+    //   message: "Sorry, export for web is currently disabled, until inkjs is updated to support the latest version of ink. You can download a previous version of Inky that supports inkjs and use that instead, although some of the latest features of ink may be missing."
+    // });
+    // return true;
+}
+
+let pendingPathToOpen = null;
+let hasFinishedLaunch = false;
+let isQuitting = false;
+
+app.on("open-file", function (event, path) {
+
+    // e.g. Drag and drop onto app to open it.
+    // "open-file" seems to come before "will-finish-launching"
+    if( !hasFinishedLaunch ) {
+        pendingPathToOpen = path;
+    }
+    
+    // Drag and drop onto app while it's already open
+    else {
+
+        // See if this root file is already open in an existing window
+        let existingWin = ProjectWindow.withMainkInkPath(path);
+        if( existingWin ) {
+            existingWin.browserWindow.focus();
+            existingWin.browserWindow.webContents.send('open-main-ink');
+        } else {
+            ProjectWindow.open(path);       
+        }
+    }
+    
+    event.preventDefault();
+});
+
+app.on('before-quit', function () {
+    // We need this to differentiate between pressing quit (which should quit) or closing all windows
+    // (which leaves the app open)
+    isQuitting = true;
+});
+
+ipc.on("project-cancelled-close", (event) => {
+    isQuitting = false;
+});
+
+ipc.on("save-data", (sender, data) => {
+    console.log("Save Data!!!")
+    const { BrowserWindow } = require('electron');
+    const parsedData = JSON.parse(data)
+    const allWindows = BrowserWindow.getAllWindows();
+
+    // Send a message to each window
+    allWindows.forEach(window => {
+      window.webContents.send('save-data', parsedData.character);
+    });
+})
+
+ipc.on("save-story", (sender, data) => {
+    console.log("Save Story Data!!!")
+    const { BrowserWindow } = require('electron');
+    const parsedData = JSON.parse(data)
+    const allWindows = BrowserWindow.getAllWindows();
+
+    // Send a message to each window
+    allWindows.forEach(window => {
+      window.webContents.send('save-story', parsedData);
+    });
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+
+    app.on('window-all-closed', function () {
+        if (process.platform != 'darwin' || isQuitting) {
+            app.quit();
+        }
+    });
+
+    AppMenus.setCallbacks({
+        new: () => {
+            ProjectWindow.createEmpty();
+        },
+        newInclude: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.newInclude();
+        },
+        open: () => {
+            ProjectWindow.open();
+        },
+        clearRecent: () => {
+            ProjectWindow.clearRecentFiles();
+            AppMenus.setRecentFiles([]);
+            AppMenus.refresh();
+        },
+        save: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.save();
+        },
+        exportJson: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.exportJson();
+        },
+        exportForWeb: () => {
+            if( inkJSNeedsUpdating() ) return;
+            var win = ProjectWindow.focused();
+            if (win) win.exportForWeb();
+        },
+        exportJSOnly: () => {
+            if( inkJSNeedsUpdating() ) return;
+            var win = ProjectWindow.focused();
+            if (win) win.exportJSOnly();
+        },
+        toggleTags: (item, focusedWindow, event) => {
+            focusedWindow.webContents.send("set-tags-visible", item.checked);
+        },
+        nextIssue: (item, focusedWindow) => {
+            focusedWindow.webContents.send("next-issue");
+        },
+        gotoAnything: (item, focusedWindow) => {
+            focusedWindow.webContents.send("goto-anything");
+        },
+        addWatchExpression: (item, focusedWindow) => {
+            focusedWindow.webContents.send("add-watch-expression");
+        },
+        showDocs: () => {
+            DocumentationWindow.openDocumentation(ProjectWindow.getViewSettings().theme);
+        },
+        showAbout: () => {
+            AboutWindow.showAboutWindow(ProjectWindow.getViewSettings().theme);
+        },
+        keyboardShortcuts: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.keyboardShortcuts();
+        },
+        stats: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.stats();
+        },
+        zoomIn: () => {
+          var win = ProjectWindow.focused();
+          if (win != null) {
+            win.zoom(2);
+            // Convert change from font size to zoom percentage
+            let zoom = ProjectWindow.getViewSettings().zoom;
+            zoom = (parseInt(zoom) + Math.floor(2*100/12)).toString();
+            ProjectWindow.addOrChangeViewSetting('zoom', zoom);
+          }
+        },
+        zoomOut: () => {
+          var win = ProjectWindow.focused();
+          if (win != null) {
+            win.zoom(-2);
+            // Convert change from font size to zoom percentage
+            let zoom = ProjectWindow.getViewSettings().zoom
+            zoom = (parseInt(zoom) - Math.floor(2*100/12)).toString();
+            ProjectWindow.addOrChangeViewSetting('zoom', zoom);
+          }
+        },
+        zoom: (zoom_percent) => {
+          var win = ProjectWindow.focused();
+          if (win != null) {
+            win.zoom(zoom_percent);
+            let zoom = zoom_percent.toString();
+            ProjectWindow.addOrChangeViewSetting('zoom', zoom)
+          }
+        },
+        insertSnippet: (focussedWindow, snippet) => {
+            if( focussedWindow )
+                focussedWindow.webContents.send('insertSnippet', snippet);
+        },
+        changeTheme: (newTheme) => {
+          AboutWindow.changeTheme(newTheme);
+          DocumentationWindow.changeTheme(newTheme);
+          ProjectWindow.addOrChangeViewSetting('theme', newTheme)
+        }
+    });
+
+    AppMenus.setRecentFiles(ProjectWindow.getRecentFiles());
+    AppMenus.setTheme(ProjectWindow.getViewSettings().theme);
+    AppMenus.setZoom(ProjectWindow.getViewSettings().zoom);
+
+    AppMenus.refresh();
+
+    ProjectWindow.setEvents({
+        onRecentFilesChanged: (recentFiles) => {
+            AppMenus.setRecentFiles(recentFiles);
+            AppMenus.refresh();
+        },
+        onProjectSettingsChanged: (settings) => {
+            settings = settings || {};
+            AppMenus.setCustomSnippetMenus(settings.customInkSnippets || []);
+            AppMenus.refresh();
+        },
+        onViewSettingsChanged: (viewSettings) => {
+            AppMenus.setTheme(viewSettings.theme);
+            AppMenus.setZoom(viewSettings.zoom);
+            AppMenus.refresh();
+        }
+    });
+
+    // Windows passed file to open on command line?
+    if (process.platform == "win32" && process.argv.length > 1 && !pendingPathToOpen) {
+        for (let i = 1; i < process.argv.length; i++) {
+            var arg = process.argv[i].toLowerCase();
+            if (arg.endsWith(".ink")) {
+                pendingPathToOpen = process.argv[1];
+                break;
+            }
+        }
+    }
+
+    // Opened Inky with specific file (e.g. drag and drop or windows command line)
+    if( pendingPathToOpen ) {
+        ProjectWindow.open(pendingPathToOpen);
+        pendingPathToOpen = null;
+    }
+    
+    // Otherwise, show new empty window
+    else {
+        ProjectWindow.createEmpty();
+    }
+
+    // Setup last stored theme
+    let theme = ProjectWindow.getViewSettings().theme;
+    AboutWindow.changeTheme(theme);
+    DocumentationWindow.changeTheme(theme);
+
+    hasFinishedLaunch = true;
+
+    // Debug
+    //w.openDevTools();
+});
+
+function finalQuit() {
+    Inklecate.killSessions();
+}
+
+forceQuitDetect.onForceQuit(finalQuit);
+electron.app.on("will-quit", finalQuit);
